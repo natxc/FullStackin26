2be125ee0d54562cd9bdcd2042ad90b603b346a8
Natalie Crow <nataliecrow@MacBook-Pro.local>
2023-10-20 07:39:22 -0600

    adding prompt engineering

---
+++ DS4A_DataEngg_Bootcamp/Streamlit_App/prompts.py
@@ -0,0 +1,98 @@
+import streamlit as st
+
+QUALIFIED_TABLE_NAME = "AIRBYTE_DATABASE.AIRBYTE_SCHEMA.ORDERS"
+METADATA_QUERY = "SELECT VARIABLE_NAME, DEFINITION FROM AIRBYTE_DATABASE.AIRBYTE_SCHEMA.BIGSUPPLYCO_ATTRIBUTES;"
+TABLE_DESCRIPTION = """
+This table consists of information about orders, products, customers, departments, and product categories.
+It contains details about individual orders, including customer ID, product ID, department ID, market, and various other attributes like order status, sales, and profit.
+The table also includes information about individual products, such as product ID, category ID, description, and price.
+Additionally, it holds data about customers, including their ID, city, country, and segment.
+There is also information about the department stores, including their ID and geographical coordinates.
+Data about product categories, including their ID and name can also be found.
+"""
+
+GEN_SQL = """
+You will be acting as an AI Snowflake SQL expert.
+Your goal is to give correct, executable Snowflake SQL queries to users.
+You will be replying to users who will be confused if you don't respond in the character of a Snowflake SQL expert.
+You are given one table, the table name is in <tableName> tag, the columns are in <columns> tag.
+The user will ask questions; for each question, you should respond and include a Snowflake SQL query based on the question and the table. 
+
+{context}
+
+Here are 7 critical rules for the interaction you must abide:
+<rules>
+1. You MUST wrap the generated Snowflake SQL queries within ``` sql code markdown in this format e.g
+```sql
+(select 1) union (select 2)
+```
+2. If I don't tell you to find a limited set of results in the sql query or question, you MUST limit the number of responses to 10.
+3. Text / string where clauses must be fuzzy match e.g ilike %keyword%
+4. Make sure to generate a single Snowflake SQL code snippet, not multiple. 
+5. You should only use the table columns given in <columns>, and the table given in <tableName>, you MUST NOT hallucinate about the table names.
+6. DO NOT put numerical at the very front of SQL variable.
+7. The queries you provide will NOT include any DML operations such as DELETE, UPDATE, or INSERT.
+</rules>
+
+Don't forget to use "ilike %keyword%" for fuzzy match queries (especially for variable_name column)
+and wrap the generated sql code with ``` sql code markdown in this format e.g:
+```sql
+(select 1) union (select 2)
+```
+
+For each question from the user, make sure to include a query in your response.
+
+Now to get started, please describe the table at a high level, DO NOT share the table name here, and share the available metrics in 2-3 sentences.
+Then provide 3 example questions using bullet points.
+"""
+
+@st.cache_data(show_spinner=False)
+def get_table_context(table_name: str, table_description: str, metadata_query: str = None):
+    table = table_name.split(".")
+    # conn = st.experimental_connection("postgresql", type="sql")
+    conn = st.experimental_connection("snowpark")
+    columns = conn.query(f"""
+        SELECT COLUMN_NAME, DATA_TYPE FROM {table[0].upper()}.INFORMATION_SCHEMA.COLUMNS
+        WHERE TABLE_SCHEMA = '{table[1].upper()}' AND TABLE_NAME = '{table[2].upper()}'
+        """,
+    )
+    columns = "\n".join(
+        [
+            f"- **{columns['COLUMN_NAME'][i]}**: {columns['DATA_TYPE'][i]}"
+            for i in range(len(columns["COLUMN_NAME"]))
+        ]
+    )
+    context = f"""
+Here is the table name <tableName> {'.'.join(table)} </tableName>
+
+<tableDescription>{table_description}</tableDescription>
+
+Here are the columns of the {'.'.join(table)}
+
+<columns>\n\n{columns}\n\n</columns>
+    
+But please keep in mind, there may be more variable_names in the metadata table than the table name.
+    """
+    if metadata_query:
+        metadata = conn.query(metadata_query)
+        metadata = "\n".join(
+            [
+                f"- **{metadata['VARIABLE_NAME'][i]}**: {metadata['DEFINITION'][i]}"
+                for i in range(len(metadata["VARIABLE_NAME"]))
+            ]
+        )
+        context = context + f"\n\nAvailable variables by variable_name:\n\n{metadata}"
+    return context
+
+def get_system_prompt():
+    table_context = get_table_context(
+        table_name=QUALIFIED_TABLE_NAME,
+        table_description=TABLE_DESCRIPTION,
+        metadata_query=METADATA_QUERY
+    )
+    return GEN_SQL.format(context=table_context)
+
+# do `streamlit run prompts.py` to view the initial system prompt in a Streamlit app
+if __name__ == "__main__":
+    st.header("System prompt for Frosty")
+    st.markdown(get_system_prompt())
