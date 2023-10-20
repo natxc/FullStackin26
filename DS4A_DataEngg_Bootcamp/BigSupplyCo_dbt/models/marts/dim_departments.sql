e976a41a50820a59548cd8099230d9541b088f56
Natalie Crow <nataliecrow@MacBook-Pro.local>
2023-10-20 07:35:28 -0600

    excluding lat and long vals

--- DS4A_DataEngg_Bootcamp/BigSupplyCo_dbt/models/marts/dim_departments.sql
+++ DS4A_DataEngg_Bootcamp/BigSupplyCo_dbt/models/marts/dim_departments.sql
@@ -1,10 +1,12 @@
+{{ config(materialized='table') }}
+
 with departments as (
     select * from {{ ref('stg_bigsupplyco__departments') }}
 ),
 
 final as (
-    select {{ dbt_utils.star(from=ref('stg_bigsupplyco__departments'), except=['etl_update_ts']) }}
+    select {{ dbt_utils.star(from=ref('stg_bigsupplyco__departments'), except=['etl_update_ts','latitude_val','longitude_val']) }}
     from departments
 )
 
-select * from final
+select distinct * from final
